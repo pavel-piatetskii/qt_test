570b9b3337ea933f8c8afe3ae42425542f062ff2
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-08-09 15:20:11 -0700

    Create hello_world GUI app

---
+++ hello_world.py
@@ -0,0 +1,36 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+
+  def __init__(self):
+
+    super().__init__()
+
+    self.hello = ["Hello World", "Hallo Welt", "Hei maailma", 
+                  "Hola Mundo", "Привет мир"]
+
+    self.button = QtWidgets.QPushButton("Click Me!")
+    self.text = QtWidgets.QLabel("Hello World", 
+                                  alignment=QtCore.Qt.AlignCenter)
+
+    self.layout = QtWidgets.QVBoxLayout(self)
+    self.layout.addWidget(self.text)
+    self.layout.addWidget(self.button)
+
+    self.button.clicked.connect(self.switch_lang)
+
+  @QtCore.Slot()
+  def switch_lang(self):
+    self.text.setText(random.choice(self.hello))
+
+
+if __name__ == "__main__":
+  app = QtWidgets.QApplication([])
+
+  widget = MyWidget()
+  widget.resize(400, 300)
+  widget.show()
+
+  sys.exit(app.exec())
