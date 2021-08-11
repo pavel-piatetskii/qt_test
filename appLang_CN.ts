de45756495431850bf18b5790d5ce2bc80b33d51
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-08-11 15:55:07 -0700

    Create new app for multi-lang test (unfinished)

---
+++ appLang_CN.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="ui/LangTestUI.py" line="28"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="ui/LangTestUI.py" line="29"/>
+        <source>please_click_left</source>
+        <translation>按摄像机列表中的任何“播放”按钮开始流式传输</translation>
+    </message>
+</context>
+</TS>
---
+++ appLang_EN.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="ui/LangTestUI.py" line="28"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="ui/LangTestUI.py" line="29"/>
+        <source>please_click_left</source>
+        <translation>Press any &apos;play&apos; button in the camera list to start streaming</translation>
+    </message>
+</context>
+</TS>
